4473f9707ea72f5ce3bfb233d7e33a7bd3f6489a
benwade91 <70988204+benn925@users.noreply.github.com>
2021-08-12 11:14:17 -0700

    turtle starting line

--- turtle_race.py
+++ turtle_race.py
@@ -1,12 +1,18 @@
 from turtle import Turtle, Screen
 import random
 
-tim = Turtle()
+
 screen = Screen()
 screen.setup(height=400, width=500)
 user_bet = screen.textinput(title="Make your bet", prompt="enter the turtle color you think will win:")
-print(user_bet)
 
-tim.goto(-200,0)
+colors = ["red", "yellow", "blue", "green", "orange", "pink"]
+
+for i in colors:
+    tim = Turtle(shape="turtle")
+    tim.color('black', i)
+    tim.name = i
+    tim.penup()
+    tim.goto(-200, (150 - (50 * colors.index(i))))
 
 screen.exitonclick()
