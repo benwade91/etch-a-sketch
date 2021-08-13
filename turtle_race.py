65ab3a80fe7af33a3ace0e738a0a17182da28533
benwade91 <70988204+benn925@users.noreply.github.com>
2021-08-13 08:20:02 -0700

    turtle betting and race functions correctly

--- turtle_race.py
+++ turtle_race.py
@@ -4,15 +4,35 @@ import random
 
 screen = Screen()
 screen.setup(height=400, width=500)
-user_bet = screen.textinput(title="Make your bet", prompt="enter the turtle color you think will win:")
+user_bet = screen.textinput(title="Make your bet", prompt="red, yellow, blue, green, orange, or pink?")
 
 colors = ["red", "yellow", "blue", "green", "orange", "pink"]
+all_turtles = []
+start_race = False
 
 for i in colors:
-    tim = Turtle(shape="turtle")
-    tim.color('black', i)
-    tim.name = i
-    tim.penup()
-    tim.goto(-200, (150 - (50 * colors.index(i))))
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color('black', i)
+    new_turtle.penup()
+    new_turtle.goto(-200, (150 - (50 * colors.index(i))))
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    start_race = True
+
+while start_race:
+    for turtle in all_turtles:
+        rand_distance = random.randint(5, 10)
+        turtle.forward(rand_distance)
+        if turtle.pos()[0] > 223:
+            start_race = False
+            winner = turtle.color()[1]
+            if user_bet.lower() == winner:
+                print(f"You win! the winner was {winner}")
+            else:
+                print(f"You lose! The winner was {winner}")
+
+
+
 
 screen.exitonclick()
